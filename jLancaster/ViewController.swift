694f4b7162b59f6c0acca682119d3c55bf14ec8b
Jake Smith <jakesmith@macbook-pro.local>
2018-09-14 08:55:07 +0100

    Initial Commit

---
+++ jLancaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jLancaster
+//
+//  Created by Jake Smith on 14/09/2018.
+//  Copyright © 2018 Nebultek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
