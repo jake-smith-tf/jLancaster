b3d69cb2ded3d0c3486c6f36b767a957fde63ec2
Jake Smith <jakesmith@Macbook-Pro.local>
2018-09-20 15:06:07 +0100

    Initial Commit

--- jLancaster/ViewController.swift
+++ jLancaster/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webview: UIWebView!
+    var timetableInfo = [[String:String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserDefaults.standard.setValue(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+        
+        let ud: UserDefaults = UserDefaults.standard
+        //let data: Data? = ud.object(forKey: "new") as? Data
+        if let data: Data = ud.object(forKey: "jLancs") as? Data,let cookie = NSKeyedUnarchiver.unarchiveObject(with: data) as? [HTTPCookie] {
+            cookie.forEach {
+                HTTPCookieStorage.shared.setCookie($0)
+                print($0)
+            }
+            
+            print("Got the cookies 🍪")
+        }
+        
+        openTimetable()
+        
+        if let url = URL(string: "https://lancaster.ombiel.co.uk/campusm/home#menu") {
+            webview.loadRequest(URLRequest(url: url))
+            webview.delegate = self
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showTimetable" {
+            let timetableVC = segue.destination as! TimetableTableViewController
+            timetableVC.timetableInfo = self.timetableInfo
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +53,42 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIWebViewDelegate {
+    
+    func openTimetable() {
+        let url = URL(string: "https://lancaster.ombiel.co.uk/campusm/sso/calendar/sso_course_timetable/2018281")
+        URLSession.shared.dataTask(with:url!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:[[String:String]]],let events = json["events"] {
+                    self.timetableInfo = events
+                    DispatchQueue.main.sync {
+                        //self.performSegue(withIdentifier: "showTimetable", sender: self)
+                    }
+                }
+                
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        webView.stringByEvaluatingJavaScript(from: "document.getElementById(\"tilesScrollBody\").childNodes[0].style.top='0px';")
+        webView.stringByEvaluatingJavaScript(from: "document.getElementById(\"header\").style.display='none';")
+        
+        let ud: UserDefaults = UserDefaults.standard
+
+            if let cookies = HTTPCookieStorage.shared.cookies {
+                if cookies.contains(where: { $0.name == "cosign" }) {
+                    let data: NSData = NSKeyedArchiver.archivedData(withRootObject: cookies) as NSData
+                    ud.set(data, forKey: "jLancs")
+                    print("SAVED COSIGN DATA")
+                }
+            }
+        
+    }
+    
+    
+}
